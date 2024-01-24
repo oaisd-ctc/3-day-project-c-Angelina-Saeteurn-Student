dc6350f64a65c6060e23406512b1a0db64b6aa79
Nicholas-Fultz-student <144163154+Nicholas-Fultz-student@users.noreply.github.com>
2024-01-24 22:08:35 +0000

    An updated main with decision controls. Bare Bones with easy breaks.

--- Program.cs
+++ Program.cs
@@ -1,53 +1,73 @@
 ﻿
+using System.Security.Cryptography.X509Certificates;
+
 class Program
 {
 
     public static void Main(string[] args)
     {
-        PlayerIntroMessage();
-
-        System.Console.WriteLine("ENTER INFO, NAME, BOSSES, QUIT");
+        Console.Clear();
+        System.Console.WriteLine("INTRO MESSAGE");
+        System.Console.WriteLine("ENTER NAME");
         string userInput = Console.ReadLine();
+
         while (userInput != "quit")
         {
+        System.Console.WriteLine("ENTER INFO, NAME, QUIT");
+        userInput = Console.ReadLine();
+
             if (userInput.ToLower() == "info")
             {
+                LoadingVisual();
+                Console.Clear();
+
                 //Information Options
                 System.Console.WriteLine("Health, Strength, Level, bosses, ...");
                 userInput = Console.ReadLine();
 
                 if (userInput.ToLower() == "health")
                 {
+                    System.Console.WriteLine("Your current health is:");
+                    System.Console.WriteLine("Enter Back to go back");
+                    userInput = Console.ReadLine();
 
                 }
+
+                else if (userInput.ToLower() == "bosses")
+                {
+                    Thread.Sleep(500);
+                    Console.Clear();
+                    System.Console.WriteLine("Which level boss do you want information on?\n Level: 1, 2, 3, or Back");
+                    userInput = Console.ReadLine();
+                }
+
                 else if (userInput.ToLower() == "strength")
                 {
 
                 }
+
                 else if (userInput.ToLower() == "level")
                 {
 
                 }
-                else if (userInput.ToLower() == "bosses")
-                {
-                    System.Console.WriteLine("Which level boss do you want information on?\n Level 1. Level 2. Level 3. or Back");
-                    userInput = Console.ReadLine();
-                    if (userInput.ToLower() == "back")
-                    {
-                        System.Console.WriteLine("Health, Strength, Level, bosses, ...");
-                userInput = Console.ReadLine();
-                    }
-                }
+
+
+
                 else
                 {
-                    System.Console.WriteLine("Not a valid response");
-                }
+                    Console.Clear();
+                    NotValidResponseMessage();
+                    userInput = Console.ReadLine();
 
+
+                }
             }
-            if (userInput.ToLower() == "Bosses")
+            if(userInput.ToLower() == "quit")
             {
-                System.Console.WriteLine("Choose boss");
+                break;
             }
+
+
         }
 
 
@@ -86,15 +106,25 @@ class Program
 
     }
 
-    public static void PlayerIntroMessage()
+    public static void LoadingVisual()
     {
-        System.Console.WriteLine("INTRO MESSAGE");
-        System.Console.WriteLine("ENTER NAME");
+        int loadingPauseTime = 300;
+        int numberOfDots = 3;
 
-        string PlayerName = Console.ReadLine();
-        Console.Clear();
-        System.Console.WriteLine("INTRO BACKGROUND STORY " + PlayerName);
-        //can use the name to implement into the story.
+        for (int i = 0; i < numberOfDots; i++)
+        {
+            System.Console.Write(".");
+            Thread.Sleep(loadingPauseTime);
+
+        }
+
     }
+
+    public static void NotValidResponseMessage()
+    {
+        System.Console.WriteLine("NOT A VALID RESPONSE.");
+        System.Console.WriteLine("ENTER INFO, NAME, BOSSES, QUIT");
+    }
+
 }
 
