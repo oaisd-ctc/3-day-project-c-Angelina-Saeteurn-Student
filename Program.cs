dfeb9d0aaf6d5c03de843e3d84bda10a3e8f0217
Nicholas-Fultz-student <144163154+Nicholas-Fultz-student@users.noreply.github.com>
2024-01-24 18:23:26 +0000

    Ways to run different choices/decisions

--- Program.cs
+++ Program.cs
@@ -5,5 +5,33 @@ class Program
     public static void Main(string[] args)
     {
 
+
+        System.Console.WriteLine("WELCOME MESSAGE");
+        System.Console.WriteLine("ENTER INFO, NAME, BOSSES, QUIT");
+        string userInput = Console.ReadLine();
+
+        while (userInput.ToLower() != "quit")
+        {
+            switch (userInput.ToLower())
+            {
+
+                case "info":
+                    System.Console.WriteLine("INFORMATION TEXT");
+                    break;
+
+                case "name":
+                    System.Console.WriteLine("RETURNING NAME TEXT");
+                    break;
+
+                case "bosses":
+                    System.Console.WriteLine("BOSS CHOICES");
+                    break;
+
+                default:
+                    System.Console.WriteLine("NOT VALID INPUT RESPONSE");
+                    break;
+            }
+            userInput = Console.ReadLine();
+        }
     }
 }
