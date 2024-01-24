3105b7c07f6be7a74d476f62236fa6c0bffbc386
Angelina-Saeteurn-Student <144161612+Angelina-Saeteurn-Student@users.noreply.github.com>
2024-01-24 18:47:47 +0000

    Commiting so i am able to have all my changes

---
+++ potions/Potion.cs
@@ -0,0 +1,16 @@
+using System.IO.Pipes;
+
+namespace potions {
+
+    public class Potion{
+        private string type;
+        private string idk;
+
+        public Potion(ItemName,ItemDescription) : base(ItemName,ItemDescription){
+            
+        }
+        public static void UseOnSelf(){
+
+        }
+    }    
+}
