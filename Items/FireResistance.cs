3483222aa431e75d65dcb08a2f53d33b2f394350
Angelina-Saeteurn-Student <144161612+Angelina-Saeteurn-Student@users.noreply.github.com>
2024-01-25 18:19:48 +0000

    Created all Potions, Edited other things

---
+++ Items/FireResistance.cs
@@ -0,0 +1,21 @@
+using System.IO.Pipes;
+
+namespace Items {
+
+    public class FireResistance: Potion
+    {
+        
+        public FireResistance (string itemName, string itemDescription) : base(itemName, itemDescription)
+        {
+            
+        }
+        public override void UseOnPlayer(Player Character)
+        {
+            Console.WriteLine("You have used the Fire Resistance Potion on yourself.");
+        }
+        public override void UseOnEnemy(Enemy Villian)
+        {
+            Console.WriteLine("You have used the Fire Resistance Potion on the enemy. (What the heck?!)");
+        }
+    }    
+}
